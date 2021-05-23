420b972617a3835cf63fbe9c4b1ac529a8df9eb7
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-23 23:26:09 +0300

    MusicApp ikinci bolum tamamlandi.

--- MusicApp/src/App.js
+++ MusicApp/src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, View, FlatList, StyleSheet} from 'react-native';
 
 import SongCard from './components/SongCard';
+import SearchBar from './components/SearchBar';
 import music_data from './music-data.json';
 
 const App = () => {
+  const [songList, setSongList] = useState(music_data);
+
   const renderSong = ({item}) => <SongCard song={item} />;
   const renderSeperator = () => <View style={styles.seperator} />;
+
+  const handleSearch = text => {
+    const filteredList = music_data.filter(song => {
+      const searchedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+      return currentTitle.indexOf(searchedText) > -1;
+    });
+
+    setSongList(filteredList);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
+        <SearchBar onSearch={handleSearch} />
         <FlatList
           keyExtractor={item => item.id}
-          data={music_data}
+          data={songList}
           renderItem={renderSong}
           ItemSeparatorComponent={renderSeperator}
         />
---
+++ MusicApp/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {TextInput, View} from 'react-native';
+
+import styles from './SearchBar.styles';
+
+const SearchBar = ({onSearch}) => {
+  return (
+    <View style={styles.container}>
+      <TextInput placeholder="Ara ..." onChangeText={onSearch} />
+    </View>
+  );
+};
+
+export default SearchBar;
