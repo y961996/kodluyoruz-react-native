b7de4bb89beef4d3b90d0b575ef889d1a75360bc
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-23 13:57:31 +0300

    State derslerine baslandi.

--- myFirstProject/src/App.js
+++ myFirstProject/src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import Card from './components/Card';
+import {SafeAreaView, Text} from 'react-native';
 
 //function App() {
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Card title="Guci Genk" text="Dincırısı mincirısım. Nincırıstır." />
-      <Card title="Sid" text="Sincırısı mincirısım. Sincırıstır." />
-      <Card title="Ratatuy" text="Herkes yemek yapabilir!" />
+      <Text>Hello State!</Text>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e0e0e0',
-  },
-});
 
 export default App;
