f875e593ff16dfdcf67baf61a1f50bc1d340fe7e
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-23 12:59:51 +0300

    Styling tamamlandi. Son elemanın bütün satiri kaplamasi eklenecek.

--- PatikaStore/src/components/StoreItemCard/StoreItemCard.style.js
+++ PatikaStore/src/components/StoreItemCard/StoreItemCard.style.js
@@ -4,13 +4,13 @@ export default StyleSheet.create({
   container: {
     backgroundColor: '#f0f0f0',
     borderRadius: 10,
-    height: Dimensions.get('window').height / 3 + 20,
+    height: '95%',
     width: '45%',
-    margin: 10,
+    margin: 7,
   },
   image: {
     height: Dimensions.get('window').height / 3 - 50,
-    width: '90%',
+    width: Dimensions.get('window').width / 2 - 40,
     borderRadius: 10,
     margin: 7,
   },
@@ -18,10 +18,12 @@ export default StyleSheet.create({
     fontWeight: 'bold',
     color: 'black',
     fontSize: 18,
+    marginLeft: 7,
   },
   priceText: {
     fontWeight: 'bold',
     fontSize: 16,
     color: 'gray',
+    marginLeft: 7,
   },
 });
