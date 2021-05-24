15ee80d0a5905985cfa249f77de66c27208f32a0
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-24 06:19:21 +0300

    TodoApp eklendi.

---
+++ TodoApp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text, FlatList} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
