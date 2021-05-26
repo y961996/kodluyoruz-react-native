ee167fed54b37910c4e7505057216328ca4666b7
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-26 16:04:18 +0300

    Router js eklendi.

--- KebapFitnessApp/index.js
+++ KebapFitnessApp/index.js
@@ -4,7 +4,7 @@
 
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Router from './src/pages/Router';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Router);
