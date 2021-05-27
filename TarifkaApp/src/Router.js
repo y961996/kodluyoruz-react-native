f9d781b0cf39ea223cf4dc33fbf199033771d6ef
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-27 22:50:41 +0300

    sayfalar router'a eklendi.

--- TarifkaApp/src/Router.js
+++ TarifkaApp/src/Router.js
@@ -4,10 +4,18 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
+import FoodCategory from './pages/FoodCategory';
+import Foods from './pages/Foods';
+import Detail from './pages/Detail';
+
+// pagelerin headerlarini customize edebilirim.
+
 const Router = () => {
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="null" component={null} />
+      <Stack.Screen name="FoodCategoryScreen" component={FoodCategory} />
+      <Stack.Screen name="FoodsScreen" component={Foods} />
+      <Stack.Screen name="DetailScreen" component={Detail} />
     </Stack.Navigator>
   </NavigationContainer>;
 };
