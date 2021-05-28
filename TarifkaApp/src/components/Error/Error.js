b7dbeb6291fccb671738c8b53024268e39866014
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-28 11:34:28 +0300

    .env eklendi.

--- TarifkaApp/src/components/Error/Error.js
+++ TarifkaApp/src/components/Error/Error.js
@@ -2,7 +2,12 @@ import React from 'react';
 import LottieView from 'lottie-react-native';
 
 const Error = () => {
-  return <LottieView source={require('../../assets/error.json')} autoPlay />;
+  return (
+    <LottieView
+      source={require('../../assets/error_exclamation.json')}
+      autoPlay
+    />
+  );
 };
 
 export default Error;
--- TarifkaApp/src/components/Loading/Loading.js
+++ TarifkaApp/src/components/Loading/Loading.js
@@ -2,7 +2,9 @@ import React from 'react';
 import LottieView from 'lottie-react-native';
 
 const Loading = () => {
-  return <LottieView source={require('../../assets/loading.json')} autoPlay />;
+  return (
+    <LottieView source={require('../../assets/loading_food.json')} autoPlay />
+  );
 };
 
 export default Loading;
